4f5643ed0917ba10cbf9a2330eb1b86a943cfc43
miklekuzichev <dev310105@gmail.com>
2023-05-04 13:32:16 +0300

    feat: add js script

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
-console.log('Hello!');
+let popup = document.querySelector('.popup');
+let buttonPopupEdit = document.querySelector('.profile__edit-button');
+let buttonPopupClose = document.querySelector('.popup__close');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__profile');
+
+// Предзаполнение формы
+nameInput.value = profileTitle.textContent;
+jobInput.value = profileSubtitle.textContent;
+
+// Обработчик «отправки» формы
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    popup.classList.remove('popup__edit-opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
+
+buttonPopupEdit.addEventListener('click', () => {
+    popup.classList.add('popup__edit-opened');
+});
+
+buttonPopupClose.addEventListener('click', () => {
+    popup.classList.remove('popup__edit-opened');
+});
