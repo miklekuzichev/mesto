fb514cca4b5e0cf852493b5eaa8ad565de8fcdce
miklekuzichev <dev310105@gmail.com>
2023-05-06 12:25:38 +0300

    fix: edit project after first codereview

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,34 @@ let buttonPopupClose = document.querySelector('.popup__close');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__profile');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_profile');
 
-// Предзаполнение формы
-nameInput.value = profileTitle.textContent;
-jobInput.value = profileSubtitle.textContent;
+function popupRemove () {
+    popup.classList.remove('popup_opened');
+}
+
+function popupOpened () {
+    popup.classList.add('popup_opened');
+}
 
 // Обработчик «отправки» формы
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    popup.classList.remove('popup__edit-opened');
+    popupRemove();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
 
 buttonPopupEdit.addEventListener('click', () => {
-    popup.classList.add('popup__edit-opened');
+    popupOpened();
+    // Предзаполнение формы
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
 });
 
 buttonPopupClose.addEventListener('click', () => {
-    popup.classList.remove('popup__edit-opened');
+    popupRemove();
 });
