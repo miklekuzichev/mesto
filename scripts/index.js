d63a2974a9986764ccf31ec07e797b1efc8bc075
miklekuzichev <dev310105@gmail.com>
2023-05-22 23:17:16 +0300

    feat: add full screen image view

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,63 @@
 let popup = document.querySelector('.popup');
-let popup_form_edit = document.querySelector('.popup_form_edit');
-//let popup_form_edit_close = popup_form_edit.querySelector('.popup__close');
-let popup_form_add = document.querySelector('.popup_form_add');
-//let popup_form_add_close = popup_form_add.querySelector('.popup__close');
+let popupFormEdit = document.querySelector('.popup_form_edit');
+let popupFormAdd = document.querySelector('.popup_form_add');
+
+let popupFormOpenImg = document.querySelector('.popup_form_open-img');
+
 
 let buttonPopupEdit = document.querySelector('.profile__edit-button');
-let buttonPopupClose = document.querySelector('.popup__close');
+let buttonPopupEditClose = document.querySelector('.popup_form_edit').querySelector('.popup__close');
+let buttonPopupAddClose = document.querySelector('.popup_form_add').querySelector('.popup__close');
+
+let buttonPopupOpenImgClose = document.querySelector('.popup_form_open-img').querySelector('.popup__close');
+
+
+
+//console.log(buttonPopupClose);
+//console.log(buttonPopup_Add_Close);
+
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_profile');
-
 let buttonPopupAdd = document.querySelector('.profile__add-button');
 
-function popupRemove () {
-    popup.classList.remove('popup_opened');
+let buttonPopupOpenImg = document.querySelector('.card__open-image');
+
+
+
+
+function popupRemove (popup_type) {
+    popup_type.classList.remove('popup_opened');
 }
 
 function popupOpened (popup_type) {
     popup_type.classList.add('popup_opened');
 }
 
+
+function popupOpenedAdd () {
+    popupFormAdd.classList.add('popup_opened');
+}
+
+function popupRemoveAdd () {
+    popupFormAdd.classList.remove('popup_opened');
+}
+
+
 // Обработчик «отправки» формы
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    popupRemove();
+    popupRemove(popupFormEdit);
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
 
 buttonPopupEdit.addEventListener('click', () => {
-    popupOpened(popup_form_edit);
+    popupOpened(popupFormEdit);
     // Предзаполнение формы
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileSubtitle.textContent;
@@ -41,14 +65,28 @@ buttonPopupEdit.addEventListener('click', () => {
 
 
 buttonPopupAdd.addEventListener('click', () => {
-    popupOpened(popup_form_add);
+    popupOpened(popupFormAdd);
     // Предзаполнение формы
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubtitle.textContent;
+    //nameInput.value = profileTitle.textContent;
+    //jobInput.value = profileSubtitle.textContent;
+});
+
+buttonPopupOpenImg.addEventListener('click', () => {
+    popupOpened(popupFormOpenImg);
+    // Предзаполнение формы
+    //nameInput.value = profileTitle.textContent;
+    //jobInput.value = profileSubtitle.textContent;
 });
 
 
-buttonPopupClose.addEventListener('click', () => {
-    popupRemove();
+buttonPopupEditClose.addEventListener('click', () => {
+    popupRemove(popupFormEdit);
 });
 
+buttonPopupAddClose.addEventListener('click', () => {
+    popupRemove(popupFormAdd);
+});
+
+buttonPopupOpenImgClose.addEventListener('click', () => {
+    popupRemove(popupFormOpenImg);
+});
