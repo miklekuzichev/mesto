70a59c1e69c15aab09d0cc71d2b9f5af7289cdb7
miklekuzichev <dev310105@gmail.com>
2023-06-25 21:36:45 +0300

    feat: Add new class FormValidator

--- scripts/validation.js
+++ scripts/validation.js
@@ -82,3 +82,99 @@ enableValidation({
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'popup__error_visible'
 });
+
+
+
+const enableValidationDev = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+
+
+
+
+class FormValidator {
+  constructor(enableValidationDev, formElement) {
+    this._enableValidationDev = enableValidationDev;
+    this._formSelector = enableValidationDev.formSelector; 
+    this._inputSelector = enableValidationDev.inputSelector; // formElement.querySelector(enableValidationDev.inputSelector);
+    this._submitButtonSelector = enableValidationDev.submitButtonSelector;
+    this._inactiveButtonClass = enableValidationDev.inactiveButtonClass;
+    this._inactiveButtonClass = enableValidationDev.inactiveButtonClass;
+    this._inputErrorClass = enableValidationDev.inputErrorClass;
+    this._errorClass = enableValidationDev.errorClass;
+    //
+    this._formElement = formElement;
+  }
+//
+  _showError() {
+    const errorElement = this._formElement.querySelector(`.${this._inputSelector.id}-error`);
+    this._inputSelector.classList.add(this._inputErrorClass);
+    errorElement.textContent = this._inputSelector.validationMessage;
+    this._inputSelector.classList.add(this._errorClass);
+  };
+//
+  _hideError() {
+    const errorElement = this._formElement.querySelector(`.${this._inputSelector.id}-error`);
+    this._inputSelector.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = '';
+  };
+//
+
+
+//
+_checkInputValidity() {
+  if (!this._inputSelector.validity.valid) {
+    this._showError();
+  } else {
+    this._hideError();
+  }
+};
+//
+
+// Проверить!
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+  })
+}; 
+//
+
+
+
+
+
+
+
+
+
+
+  enableValidation() {
+    console.log(_enableValidationDev);
+
+    this._formElement.addEventListener('submint', function (evt) {
+      evt.preventDefault();
+      //console.log(this._enableValidationDev);
+    });
+
+  };
+
+
+
+
+
+
+}
+
+
+const test = new FormValidator(enableValidationDev, );
+
+test.enableValidation();
+
+
