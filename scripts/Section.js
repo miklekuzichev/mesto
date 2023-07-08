cf9aec5205da0e0c0ddc331f918ced0aae7849e7
miklekuzichev <dev310105@gmail.com>
2023-07-08 14:14:22 +0300

    fix: edit Section class

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,7 +1,32 @@
-
 export default class Section {
-    constructor() {
-        
+    constructor({ data, renderer }, containerSelector) {
+      this._renderedItems = data;
+      this._container = document.querySelector(containerSelector);
+      this._renderer = renderer;
     }
+  
+    setItem(element) {
+      this._container.append(element);
+    }
+  
+    clear() {
+      this._container.innerHTML = '';
+    }
+  
+    renderItems() {
+      this.clear();
+  
+      this._renderedItems.forEach(item => {
+        this._renderer(item);
+      });
+    }
+  }
 
-}
+
+
+//Этот класс:
+//Первым параметром конструктора принимает объект с двумя свойствами: items и renderer. Свойство items — это массив данных, которые нужно добавить на страницу при инициализации класса. Свойство renderer — это функция, которая отвечает за создание и отрисовку данных на странице.
+//Второй параметр конструктора — селектор контейнера, в который нужно добавлять созданные элементы.
+//Содержит публичный метод, который отвечает за отрисовку всех элементов. Отрисовка каждого отдельного элемента должна осуществляться функцией renderer.
+//Содержит публичный метод addItem, который принимает DOM-элемент и добавляет его в контейнер.
+//У класса Section нет своей разметки. Он получает разметку через функцию-колбэк и вставляет её в контейнер.
