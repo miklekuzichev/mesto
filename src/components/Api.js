5e5952aecea0b7ed8612939688ff2f57ac0c2546
miklekuzichev <dev310105@gmail.com>
2023-07-26 23:44:40 +0300

    fix: edit after code-review - add new method _request in Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -9,103 +9,100 @@ export default class Api {
 //
 // Метод проверки ответа сервера
 //
-    _checkResponse(res) {
-        if(res.ok) {
-            return res.json();
-        } else {
-            return Promise.reject(`Ошибка: ${res.status}`);
-        }
+   _checkResponse(res) {
+    if(res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
     }
+  }
+//
+// Метод проверки ответа сервера
+//
+  _request(url, options) {
+    return fetch(`${this._baseUrl}/${url}`, options)
+      .then(this._checkResponse)
+  } 
 //
 // Метод получения картинок с сервера
 //
-    getInitialCards() {
-        return fetch(`${this._baseUrl}/cards`, {
-            headers: this._headers
-          })
-            .then(res => this._checkResponse(res))
-    }
+  getInitialCards() {
+    return this._request('cards', { headers: this._headers });
+}
 //
 // Метод изменения аватара пользователя
 //  
-    editAvatar(data) {
-        return fetch(`${this._baseUrl}/users/me/avatar`, {
-            method: 'PATCH',
-            headers: this._headers,
-            body: JSON.stringify(
-                {avatar: data.imagelink
-            })
-          })
-            .then(res => this._checkResponse(res))
-    }
+  editAvatar(data) {
+    return this._request('users/me/avatar', 
+      { method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify(
+          {avatar: data.imagelink
+          }) 
+      });
+}
 //
 // Метод получения информации о пользователе
 //
-    getUserInfo() {
-        return fetch(`${this._baseUrl}/users/me`, {
-          headers: this._headers
-        })
-          .then(res => this._checkResponse(res))
-      }
+  getUserInfo() {
+    return this._request('users/me', 
+      { headers: this._headers,
+      });
+  }
  //
  // Метод загрузки новой карточки на сервер
  //   
-    addCard(data) {
-        return fetch(`${this._baseUrl}/cards`, {
-          method: 'POST',
-          headers: this._headers,
-          body: JSON.stringify(
-            {name: data.imagename,
-             link: data.imagelink
-          })
-        })
-          .then(res => this._checkResponse(res));
-    }
+  addCard(data) {
+    return this._request('cards', 
+    { method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(
+        {name: data.imagename,
+          link: data.imagelink
+        }) 
+    });
+  }
 //
 // Метод Изменения информации о пользователе
 //
-    editUserInfo(data) {
-        return fetch(`${this._baseUrl}/users/me`, {
-          method: 'PATCH',
-          headers: this._headers,
-          body: JSON.stringify(
-            {name: data.username,
-             about: data.userprofile
-          })
+  editUserInfo(data) {
+    return this._request('users/me', 
+    { method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(
+        {name: data.username,
+          about: data.userprofile
         })
-          .then(res => this._checkResponse(res));
-    }
+    });
+  }
 //
 // Метод удаления карточки с сервера
 //
-    deleteCard(cardId) {
-        return fetch(`${this._baseUrl}/cards/${cardId}`, {
-          method: 'DELETE',
-          headers: this._headers
-        })
-          .then(res => this._checkResponse(res));
-    }
+  deleteCard(cardId) {
+    return this._request(`cards/${cardId}`, 
+    { method: 'DELETE',
+      headers: this._headers
+    });
+  } 
 //
 // Метод установки лайка
 //
-    makeLike(cardId) {
-        return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-          method: 'PUT',
-          headers: this._headers
-        })
-          .then(res => this._checkResponse(res));
-    }
+  makeLike(cardId) {
+    return this._request(`cards/${cardId}/likes`, 
+    { method: 'PUT',
+      headers: this._headers
+    });
+  } 
 //
 // Метод удаления лайка
 //
-    deleteLike(cardId) {
-        return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-          method: 'DELETE',
-          headers: this._headers
-        })
-          .then(res => this._checkResponse(res));
-    }
-  }
+  deleteLike(cardId) {
+    return this._request(`cards/${cardId}/likes`, 
+    { method: 'DELETE',
+      headers: this._headers
+    });
+}
+}
 //
 //
 //
